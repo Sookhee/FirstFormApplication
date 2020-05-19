494eecc789ecebc322387bf94588f86a3a917234
Sookhee <s2018s38@e-mirim.hs.kr>
2020-05-19 12:29:25 +0900

    버튼 동적 생성 실습

--- FirstFormApplication/Form1.Designer.cs
+++ FirstFormApplication/Form1.Designer.cs
@@ -28,13 +28,34 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.myButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // myButton
+            // 
+            this.myButton.Location = new System.Drawing.Point(12, 12);
+            this.myButton.Name = "myButton";
+            this.myButton.Size = new System.Drawing.Size(138, 34);
+            this.myButton.TabIndex = 0;
+            this.myButton.Text = "정민지";
+            this.myButton.UseVisualStyleBackColor = true;
+            this.myButton.Click += new System.EventHandler(this.myButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.myButton);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button myButton;
     }
 }
 
